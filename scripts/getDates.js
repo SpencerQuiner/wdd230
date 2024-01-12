54c66d19e4e3d7260b9dd82ea59fa4d0d7dc919e
SpencerQuiner <spencerquiner@gmail.com>
2024-01-12 13:58:17 +0000

    updated home link on personal webpage

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,7 +1,7 @@
 const date = new Date();
 let copywrite = date.getFullYear();
 let lastmodified = new Date(document.lastModified);
-let lastmod = lastmodified.toLocaleDateString('en-us', {year:"numeric", month:"short", day:"numeric"});
+let lastmod = lastmodified.toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"});
 console.log(lastmod)
 document.querySelector('#copywrite').textContent = `\xA9${copywrite} \u2022 Spencer Quiner \u2022 Spain`;
 document.querySelector('#lastmod').textContent = `Last Modified: ${lastmod}`;
