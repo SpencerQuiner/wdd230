0599bfeace8117fa7aa978e2d0dd5f8f9052c9b9
SpencerQuiner <spencerquiner@gmail.com>
2024-01-17 11:35:08 +0000

    finished week 02 learning activities.

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,14 +2,24 @@ const input = document.querySelector("input");
 const button = document.querySelector("button");
 const list = document.querySelector("#list");
 
-button.addEventListener(click , ()=>{
-    if(input.value != ''){
-        
-        
+
+button.addEventListener("click" , ()=>{
+    if(input.value != ""){
+        const li = document.createElement("li");
+        li.textContent = input.value;
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "❌";
+        li.append(deleteBtn);
+        list.append(li);
+        input.value= "";
+        deleteBtn.addEventListener("click", ()=>{
+            list.removeChild(li);
+            input.focus();
+        });   
     }
     else {
-        let message = document.createElement('span').textContent= `Please enter a book and chapter reference. `;
-        document.querySelector("label").appendChild(message);
+        alert( `Please enter a book and chapter reference. `);
         input.focus();
     }
+
 });
