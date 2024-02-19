ac059ed83133a4e43b799d78414f20c3c1f962a1
SpencerQuiner <spencerquiner@gmail.com>
2024-02-19 10:20:52 +0000

    updat footer script

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -15,7 +15,7 @@ let copywrite = date.getFullYear();
 let lastmodified = new Date(document.lastModified);
 let lastmod = lastmodified.toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"});
 //console.log(lastmod)
-document.querySelector('#copywrite').textContent = `Copywrite \xA9${copywrite} \u2022 Spencer Quiner`;
+document.querySelector('#copywrite').textContent = `Copywrite \xA9${copywrite} \u2022 Spencer Quiner \u2022 WDD 230`;
 document.querySelector('#lastmod').textContent = `Last Modified: ${lastmod}`;
 
 const currentDate = date.toLocaleDateString('en-us', { year:"numeric", month:"short", day:"numeric"});
